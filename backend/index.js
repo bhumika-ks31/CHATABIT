86fedf817af9b766db78029bf39d1c9dea555658
Bhumika kashyap <tilantajagra@gmail.com>
2025-10-03 12:21:02 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,31 +1,40 @@
-import express from "express"
-import dotenv from "dotenv"
-import connectDb from "./config/db.js"
-import authRouter from "./routes/auth.routes.js"
-import cookieParser from "cookie-parser"
-dotenv.config()
-import cors from "cors"
-import userRouter from "./routes/user.routes.js"
-import messageRouter from "./routes/message.routes.js"
-import { app, server } from "./socket/socket.js"
+import express from "express";
+import dotenv from "dotenv";
+import connectDb from "./config/db.js";
+import authRouter from "./routes/auth.routes.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import userRouter from "./routes/user.routes.js";
+import messageRouter from "./routes/message.routes.js";
+import { app, server } from "./socket/socket.js";
+
+dotenv.config();
+
+
+const port = process.env.PORT || 5000;
+
 
 app.use(cors({
   origin: [
-    "http://localhost:5173", 
+    "http://localhost:5173",
     "https://chatabit-frontend4.onrender.com"
   ],
   credentials: true
 }));
 
-app.use(express.json())
-app.use(cookieParser())
-app.use("/api/auth",authRouter)
-app.use("/api/user",userRouter)
-app.use("/api/message",messageRouter)
+app.use(express.json());
+app.use(cookieParser());
 
 
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
+app.use("/api/message", messageRouter);
 
-server.listen(port,()=>{
-    connectDb()
-    console.log("server started")
-})
+server.listen(port, async () => {
+  try {
+    await connectDb();
+    console.log(`Server started on port ${port}`);
+  } catch (err) {
+    console.error("DB connection error:", err);
+  }
+});
